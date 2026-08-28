1b59007381aeb5f8f8b78e5117c161edf2ce74d4
Istikhar naz <muhammadmustf564@gmail.com>
2026-08-28 20:53:27 +0500

    Delete Automation directory

--- Automation/Log-File-Scanner/log_scanner.py
+++
@@ -1,6 +0,0 @@
-keywords = ["error", "failed", "warning"]
-
-with open("log-file.txt", "r") as file:
-    for line_number, line in enumerate(file, start=1):
-        if any(word in line.lower() for word in keywords):
-            print(f"Line {line_number}: {line.strip()}")
--- Automation/README.md
+++
@@ -1,3 +0,0 @@
-## ```Log-File-Scanner```
-
-A Python script that scans log files for common error, warning, and failed messages. It performs a case-insensitive search and displays the matching lines along with their line numbers.
