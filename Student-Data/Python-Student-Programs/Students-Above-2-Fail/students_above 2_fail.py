e18a601f4277ecf72a25fe0712e57cedc360e3ea
Istikhar naz <muhammadmustf564@gmail.com>
2026-08-29 16:59:06 +0500

    Delete Student-Data/Python-Student-Programs directory

--- Student-Data/Python-Student-Programs/Students-Above-2-Fail/students_above 2_fail.py
+++
@@ -1,9 +0,0 @@
-student = {
-    "aslam":3,
-    "ali": 4,
-    "farhan":2
-}
-for student_name in student:
-  fail_count = student[student_name]
-  if fail_count > 2:
-    print(student_name ,fail_count)
