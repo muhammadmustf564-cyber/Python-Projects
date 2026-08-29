204b08248f20f78644548ff57b131cf4947605c0
Istikhar naz <muhammadmustf564@gmail.com>
2026-08-29 18:53:03 +0500

    Add files via upload

---
+++ Data-Processing/Student-Data-Cleaning/student_data.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('students_dirty_data.csv')
+
+# Clean column names (IMPORTANT)
+df.columns = df.columns.str.strip()
+
+# Fill missing Age with mode
+age_mode = df['Age'].mode()[0]
+df['Age'] = df['Age'].fillna(age_mode)
+
+# Fill missing Marks with mean
+marks_mean = df['Marks'].mean()
+df['Marks'] = df['Marks'].fillna(marks_mean)
+
+#data of karachi students
+karachi_students = df[df['City'] == 'Karachi']
+print("Students of Karachi:")
+print(karachi_students)
+
+#data of lahore students
+Lahore_students = df[df['City'] == 'Lahore']
+print("Students of Lahore:")
+print(Lahore_students)
+
+#data of all female students
+female_students = df[df['Gender'] == 'F']
+print("Female_Students:")
+print(female_students)
+
+#data of all male students
+male_students = df[df['Gender'] == 'M']
+print("Male_Students:")
+print(male_students)
+
+#marks greater than 80
+marks_greater = df[df['Marks'] > 85]
+print("Marks greater than 85:")
+print(marks_greater)
+
+#sorted by marks
+sorted_df = df.sort_values(by = 'Marks')
+print("Sorted marks in ascending:")
+print(sorted_df)
+
+#top 5 students
+top_5 = df.sort_values(by='Marks', ascending=False).head(5)
+print("Top 5 Students:")
+print(top_5)
+
+#lowest 5 students
+lowest_5 = df.sort_values(by = 'Marks',ascending=True).head(5)
+print("Lowest 5 students:")
+print(lowest_5)
+
+#new file
+df.to_excel("student_clean_data.xlsx", index=False)
+
